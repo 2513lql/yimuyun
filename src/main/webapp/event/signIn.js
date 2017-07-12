ef6c3a619dfa84911fdc7799d25a15a884d64cad
weixuan168 <864214964@qq.com>
2017-07-12 16:50:59 +0800

    update

--- src/main/webapp/event/signIn.js
+++ src/main/webapp/event/signIn.js
@@ -3,19 +3,20 @@
  */
 
 $("#getCode").click(function () {
-    var  phone = $("phone").val();
+    var  phone = $("#phone").val();
     if(phone == ""){
         return;
     }
     $.ajax({
         async: false,
-        url: "http://115.28.109.174:8383/yimu/mobile/logisticsReceiver/v1.0.0/mobile/" + phone,
+        // url: "http://115.28.109.174:8383/yimu/mobile/logisticsReceiver/v1.0.0/mobile/" + phone,
+        url: "http://localhost:8081/mobile/logisticsReceiver/v1.0.0/mobile/" + phone,
         type: "GET",
         dataType : "jsonp",
         jsonp:"_callback",
         success: function (data) {
             console.log(data['head']['msg'])
-            window.location.href = "signDeta.jsp";
+            // window.location.href = "signDeta.jsp";
             // if(data['body']['result'] == "true"){
             //     $("#queryResult").html("你的产品为 <span>真</span>");
             // }else {
