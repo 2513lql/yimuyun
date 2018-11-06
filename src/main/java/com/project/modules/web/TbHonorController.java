9864f3d25890c3525cfcdc4e548fba4bed79a31c
liqilong <liqilong@meituan.com>
2018-11-06 19:11:11 +0800

    11

--- src/main/java/com/project/modules/web/TbHonorController.java
+++ src/main/java/com/project/modules/web/TbHonorController.java
@@ -17,9 +17,7 @@ import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
@@ -37,6 +35,7 @@ import java.util.Map;
 public class TbHonorController extends BaseController {
 
 
+	@ResponseBody
 	@RequestMapping("/page")
 	public Map<String,Object> getHonors(Integer pageNo){
 		String adminPictureHead = ConfigUtil.getAdminUrlHead();
